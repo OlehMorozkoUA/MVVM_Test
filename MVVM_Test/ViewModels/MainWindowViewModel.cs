464b6bd2828fdff8ac0eabdfa4763ff8507508d8
OlehMorozkoUA <oleh.morozko@gmail.com>
2022-02-10 21:48:54 +0200

    Added Header of window

--- MVVM_Test/ViewModels/MainWindowViewModel.cs
+++ MVVM_Test/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVVM_Test.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace MVVM_Test.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModelBase
     {
+        #region Header of window
+        private string _title = "Analysing of statistic";
+
+        /// <summary>
+        /// Title for window
+        /// </summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+        #endregion
     }
 }
